7513eb2913d0fcaf18d0c3ebc0709ac36f412a29
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-05 08:59:40 +0200

    feat: add Validators for Auth

---
+++ backend/RouteXY/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using RouteXY.Api.Requests;
+
+namespace RouteXY.Api.Validators.Auth;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format")
+            .MaximumLength(150);
+        
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters");
+    }
+}
---
+++ backend/RouteXY/Validators/Auth/RefreshTokenRequestValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+using RouteXY.Api.Requests;
+
+namespace RouteXY.Api.Validators.Auth;
+
+public class RefreshTokenRequestValidator : AbstractValidator<RefreshTokenRequest>
+{
+    public RefreshTokenRequestValidator()
+    {
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty().WithMessage("Refresh token is required");
+    }
+}
