6e5fd339358df263645c357bbe5049e0a200dda4
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-05 08:16:44 +0200

    feat: set GenerateDocumentationFile to true and configure swagger documentation in Program.cs

--- backend/RouteXY/Program.cs
+++ backend/RouteXY/Program.cs
@@ -1,7 +1,22 @@
+using System.Reflection;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "RouteXY API",
+        Version = "v1",
+        Description = "Courier management system API"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
