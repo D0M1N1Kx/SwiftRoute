cec23f8302372551b9e84f0cde984d1fedd79474
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-08 19:17:24 +0200

    feat: add _hubContext (LocationHub) and add it to constuctor

--- backend/RouteXY/Services/LocationService.cs
+++ backend/RouteXY/Services/LocationService.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.SignalR;
 using RouteXY.Api.Data;
+using RouteXY.Api.Hubs;
 
 namespace RouteXY.Api.Services;
 
 public class LocationService
 {
     private readonly AppDbContext _db;
+    private readonly IHubContext<LocationHub> _hubContext;
 
-    public LocationService(AppDbContext db)
+    public LocationService(AppDbContext db, IHubContext<LocationHub> hubContext)
     {
         _db = db;
+        _hubContext = hubContext;
     }
 }
