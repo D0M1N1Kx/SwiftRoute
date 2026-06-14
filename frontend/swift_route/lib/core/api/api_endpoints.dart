279daaaa18656a907ea5d61e7eb1d6cb42368b26
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-14 14:13:11 +0200

    feat: add api_endpoints.dart

---
+++ frontend/swift_route/lib/core/api/api_endpoints.dart
@@ -0,0 +1,20 @@
+class ApiEndpoints {
+  static const String login = '/auth/login';
+  static const String logout = '/auth/logout';
+  static const String refresh = '/auth/refresh';
+
+  static const String users = '/users';
+  static String userById(String id) => '/users/$id';
+
+  static const String orders = '/orders';
+  static String orderById(String id) => '/orders/$id';
+  static String assignCourier(String id) => '/orders/$id/assign';
+  static String updateOrderStatus(String id) => '/orders/$id/status';
+
+  static const String warehouses = '/warehouses';
+  static String warehouseById(String id) => '/warehouses/$id';
+  static String warehouseInventory(String id) => '/warehouses/$id/inventory';
+
+  static const String courierLocations = '/couriers/locations';
+  static const String updateLocation = '/couriers/location';
+}
