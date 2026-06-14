781805d96c9a5b5ef3601cfacbcfeaa07994d960
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-14 14:21:40 +0200

    feat: add auth_model.dart

---
+++ frontend/swift_route/lib/core/models/auth_model.dart
@@ -0,0 +1,21 @@
+import 'package:swift_route/core/models/user_model.dart';
+
+class AuthModel {
+  final String accessToken;
+  final String refreshToken;
+  final UserModel user;
+
+  AuthModel({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.user
+  });
+
+  factory AuthModel.fromJson(Map<String, dynamic> json) {
+    return AuthModel(
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+      user: UserModel.fromJson(json['user']),
+    );
+  }
+}
