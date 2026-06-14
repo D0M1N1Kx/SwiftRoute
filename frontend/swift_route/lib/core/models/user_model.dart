c40111bf8c10f90b1a80e882e86788ec33e6846d
D0M1N1Kx <milkovicsd32@gmail.com>
2026-06-14 14:18:48 +0200

    feat: add user_model.dart

---
+++ frontend/swift_route/lib/core/models/user_model.dart
@@ -0,0 +1,31 @@
+class UserModel {
+  final String id;
+  final String fullName;
+  final String email;
+  final String role;
+  final String? phone;
+  final bool isActive;
+  final DateTime createdAt;
+
+  UserModel({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.role,
+    this.phone,
+    required this.isActive,
+    required this.createdAt
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      fullName: json['fullName'],
+      email: json['email'],
+      role: json['role'],
+      phone: json['phone'],
+      isActive: json['isActive'],
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
+}
